6182f1b7cf2f77b1c994de0c9998ed7518de8c22
Alisa Ng <alisang37@gmail.com>
2018-02-21 14:01:12 +0000

    rails yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [ :index, :new, :create, :show ] do
+    resources :reviews, only: [ :new, :create ]
+  end
+
+  namespace :admin do
+    resources :restaurants, only: [ :edit, :update, :destroy]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,31 @@ restaurants_attributes = [
   {
     name:         'Dishoom',
     address:      '7 Boundary St, London E2 7JE',
-    phone_number:  12 34 56 78 90,
+    phone_number:  '12 34 56 78 90',
     category:        'french',
   },
   {
     name:         'Pizza East',
     address:      '56A Shoreditch High St, London E1 6PQ',
-    phone_number:  12 34 56 78 99,
+    phone_number:  '12 34 56 78 99',
     category:       'italian',
-  }
+  },
   {
     name:         'PF Changs',
     address:      'Chinatown',
-    phone_number:  12 34 56 78 89,
+    phone_number:  '12 34 56 78 89',
     category:       'chinese',
-  }
+  },
   {
     name:         'Sushi Samba',
     address:      'SOHO',
-    phone_number:  12 34 56 68 99,
+    phone_number:  '12 34 56 68 99',
     category:       'japanese',
-  }
+  },
   {
     name:         'Pizza East',
     address:      '56A Shoreditch High St, London E1 6PQ',
-    phone_number:  12 34 56 78 99,
+    phone_number:  '12 34 56 78 99',
     category:       'italian',
   }
 ]
