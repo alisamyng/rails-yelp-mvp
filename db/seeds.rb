c3c5eff76c864d24a39cc4960b62691f0ba46ed6
Alisa Ng <alisang37@gmail.com>
2018-02-21 11:33:37 +0000

    Change phone number from integer to string

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  12 34 56 78 90,
+    category:        'french',
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  12 34 56 78 99,
+    category:       'italian',
+  }
+  {
+    name:         'PF Changs',
+    address:      'Chinatown',
+    phone_number:  12 34 56 78 89,
+    category:       'chinese',
+  }
+  {
+    name:         'Sushi Samba',
+    address:      'SOHO',
+    phone_number:  12 34 56 68 99,
+    category:       'japanese',
+  }
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  12 34 56 78 99,
+    category:       'italian',
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
